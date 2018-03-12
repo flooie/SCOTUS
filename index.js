5fb046c34a99c1de9134143d817fd05fbacf2c13
William Palin <flooie@users.noreply.github.com>
2018-03-12 12:18:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function getspeechone(){
 (function() {
 
     var x =  "PALIN: adsfadsf asdf asdf asdf asdf adsf adsf adsf asdf adsf adsfdasfdfdsafsdf adsf asdf "
-    var y =  getspeechtwo()
+    var y =  "RBG: I dissent"
 
     data.parties = [ {name: "democrat", speeches: x }, {name: "republican", speeches: y } ].map(party);
 
